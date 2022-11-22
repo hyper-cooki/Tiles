336fe7de12b4b906b618ebcb8334014ed837db70
CookiCode <cookikit@gmail.com>
2022-11-23 09:42:25 +1300

    Update

---
+++ docs/3d/Tiles3dBetaScripts/main.js
@@ -0,0 +1,82 @@
+if (sessionStorage.tiletypes) {
+    TILE_TYPES = JSON.parse(sessionStorage.getItem("tiletypes"))
+} else {
+// Possible tile types
+TILE_TYPES = [
+    { id: 0, colour: 'rgba(0,0,0,0)', tileImage: 'square' },
+    { id: 1, colour: 'rgba(255,255,255,1)', tileImage: 'square' },
+]
+}
+    
+if (sessionStorage.tilemap) {
+mapData = JSON.parse(sessionStorage.getItem("tilemap"))
+} else {
+// Map tile data
+mapData = [[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+], [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+], [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]]
+}
+
+const scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffa500 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+cube.position.set(0, 0, 0);
+
+const cube2 = new THREE.Mesh( geometry, material );
+scene.add( cube2 );
+cube2.position.set(1, 0, 0);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+function resize() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
